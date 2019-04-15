0a6bf92d51f5d0d9dd1848a4d398bb2e82ac7d39
computerclass678 <48740355+computerclass678@users.noreply.github.com>
2019-04-15 13:12:53 +0900

    Week 2 Commit
    Save method
    Quit method
    Show All Deleted

--- capstone.py
+++ capstone.py
@@ -1,4 +1,6 @@
 import tkinter 
+import tkinter.messagebox
+import pickle
 
 root = tkinter.Tk()
 root.configure(bg = "white")
@@ -6,77 +8,102 @@ root.title("AP Economics Review Guide")
 root.geometry("3000x3000")
 
 
+class Concept():
+    def __init__(self, definition, explanation, example): 
+        self.definition = definition
+        self.explanation = explanation
+        self.example = example
 
-def concept_1(): 
-    pass 
-
-def concept_2(): 
-    pass 
-
-def concept_3(): 
-    pass 
-
-def update_listbox():
-    pass
-
-def clear_listbox(): 
-    pass 
-
-def show_all():
-    pass
-
+concept_1 = Concept("def1", "exp1", "exa1")
+concept_2 = Concept("def2", "exp2", "exa2")
+concept_3 = Concept("def3", "exp3", "exa3")
+concepts = [concept_1, concept_2, concept_3]
 
 def delete_one():
-    pass 
+    lb_explanation.delete("active")
+    print(explanation)
+
+def show_concept_one():
+    lb_explanation.delete(0, "end")
+    for concept in concepts:
+        print("Definition: {}".format(concept.definition))
+        print("Explanation: {}".format(concept.explanation))
+        print("Example: {}".format(concept.example))
+    lb_explanation.insert("end", concept_1.definition, concept_1.explanation, concept_1.example)
+
+
+def show_concept_two():
+    lb_explanation.delete(0, "end")
+    for concept in concepts:
+        print("Definition: {}".format(concept.definition))
+        print("Explanation: {}".format(concept.explanation))
+        print("Example: {}".format(concept.example))
+    lb_explanation.insert("end", concept_2.definition, concept_2.explanation, concept_2.example)
+
+def show_concept_three():
+    lb_explanation.delete(0, "end")
+    for concept in concepts:
+        print("Definition: {}".format(concept.definition))
+        print("Explanation: {}".format(concept.explanation))
+        print("Example: {}".format(concept.example))
+    lb_explanation.insert("end", concept_3.definition, concept_3.explanation, concept_3.example)
 
 def save_file():
-    pass 
+    data = text_input
+    data = pickle.dump(data, open("Computer Science.dat", "wb"))
+    messagebox.showinfo("Please Confirm", "Where do you want to save?")
 
 def exit():
+    confirm = tkinter.messagebox.askyesno("Confirm", "Are you sure you want to quit? Please make sure to save your notes before you quit.")
+    if confirm: 
+        global root
+        root.quit()
+
+def dates():
     pass
-    
 
+lbl_button = tkinter.Label(root, text = "Button", bg = "White")
+lbl_button.grid(row = 0, column = 0)
 
+btn_concept_1 = tkinter.Button(root, text = "Foreign Exchange Market", fg = "black", width = 20, height = 3, command = show_concept_one) 
+btn_concept_1.grid(row = 1, column = 0)
 
+btn_concept_2 = tkinter.Button(root, text = "Game Theory", fg = "black", width = 16, height = 3, command = show_concept_two) 
+btn_concept_2.grid(row = 2, column = 0)
 
+btn_concept_3 = tkinter.Button(root, text = "Marginal Analysis", fg = "black", width = 16, height = 3, command = show_concept_three) 
+btn_concept_3.grid(row = 3, column = 0)
 
-
-
-btn_concept_1 = tkinter.Button(root, text = "Foreign Exchange Market", fg = "black", width = 20, command = concept_1) 
-btn_concept_1.grid(row = 0, column = 0)
-
-btn_concept_2 = tkinter.Button(root, text = "Game Theory", fg = "black", width = 16, command = concept_2) 
-btn_concept_2.grid(row = 1, column = 0)
-
-btn_concept_3 = tkinter.Button(root, text = "Marginal Analysis", fg = "black", width = 16, command = concept_3) 
-btn_concept_3.grid(row = 2, column = 0)
-
-btn_show_all= tkinter.Button(root, text = "Show All", fg = "black", width = 16, command = show_all) 
-btn_show_all.grid(row = 3, column = 0)
-
-btn_delete_one = tkinter.Button(root, text = "Delete One", fg = "black", width = 16, command = delete_one) 
+btn_delete_one = tkinter.Button(root, text = "Delete One", fg = "black", width = 16, height = 3, command = delete_one) 
 btn_delete_one.grid(row = 4, column = 0)
 
-btn_save_file = tkinter.Button(root, text = "Save File", fg = "black", width = 16, command = save_file) 
+btn_save_file = tkinter.Button(root, text = "Save File", fg = "black", width = 16, height = 3, command = save_file) 
 btn_save_file.grid(row = 5, column = 0)
 
-btn_quit = tkinter.Button(root, text = "Exit", fg = "black", width = 16, command = exit) 
-btn_quit.grid(row = 6, column = 0)
+btn_exit = tkinter.Button(root, text = "Exit", fg = "black", width = 16, height = 3, command = exit) 
+btn_exit.grid(row = 6, column = 0)
 
 lbl_title = tkinter.Label(root, text = "Countdown Dates", bg = "White")
-lbl_title.grid(row = 8, column = 0)
+lbl_title.grid(row = 7, column = 0)
 
 lb_dates = tkinter.Listbox(root, height = 2) 
-lb_dates.grid(row = 9, column = 0)
+lb_dates.grid(row = 7, column = 0)
 
-lb_explanation = tkinter.Listbox(root, width = 110, height = 20) 
-lb_explanation.grid(row = 1, column = 1)
+lbl_explanation = tkinter.Label(root, text = "Explanation", bg = "White")
+lbl_explanation.grid(row = 0, column = 4)
 
-lb_graph = tkinter.Listbox(root, width = 50, height = 20) 
-lb_graph.grid(row = 0, column = 3)
+lb_explanation = tkinter.Listbox(root, width = 100, height = 48) 
+lb_explanation.grid(row = 1, column = 4, rowspan = 9)
 
-lb_note = tkinter.Listbox(root, width = 50, height = 20) 
-lb_note.grid(row = 1, column = 3)
+lb_graph = tkinter.Listbox(root, width = 58, height = 20) 
+lb_graph.grid(row = 0, column = 8, rowspan = 5)
+
+text_input = tkinter.Text(root, width = 58, bg="Yellow", bd=5)
+text_input.grid(row = 7, column = 8, rowspan = 2) 
+
+
+
+
 
 root.mainloop() 
 
