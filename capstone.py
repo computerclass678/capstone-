63c755b7b1f009bfd8f3fe8cd878d87fbae479dd
computerclass678 <48740355+computerclass678@users.noreply.github.com>
2019-04-25 09:53:45 +0900

    Add files via upload

--- capstone.py
+++ capstone.py
@@ -1,13 +1,13 @@
 import tkinter 
 from tkinter import messagebox
 import pickle
-import datetime
 
 root = tkinter.Tk()
 root.configure(bg = "white")
 root.title("AP Economics Review Guide") 
 root.geometry("3000x3000")
 
+to_delete = ""
 
 class Concept():
     def __init__(self, definition, explanation, example): 
@@ -15,48 +15,57 @@ class Concept():
         self.explanation = explanation
         self.example = example
 
-concept_1 = Concept("""
-A market structure with one firm producing a unique product, 
-determine the product price with high barriers to enter 
-and exit the market. 
-
-Natural Monopoly: 
-An industry makes the price cheaper and produces efficiently 
-by having a monopoly than several smaller competing firms. 
-""", "exp1", "exa1")
+concept_1 = Concept("Monopoly: a market structure with one firm producing a unique product, determine the product price with high barriers to enter and exit the market. Natural Monopoly: An industry makes the price cheaper and produces efficiently by having a monopoly than several smaller competing firms.", "Examples: Facebook, Paypal, Electric Power Companies, Phone Companies", "")
 concept_2 = Concept("def2", "exp2", "exa2")
-concept_3 = Concept("def3", "exp3", "exa3")
-concepts = [concept_1, concept_2, concept_3]
+concepts = [concept_1, concept_2]
+
+def set_delete_definition(event = None):
+    print("DEFINITION SET")
+    global to_delete
+    to_delete = "definition"
+    lbl_definition.configure(fg="red")
+
+def set_delete_example(event = None):
+    print("EXAMPLE SET")
+    global to_delete
+    to_delete = "example"
+    
+def set_delete_explanation(event = None):
+    print("EXPLANATION SET")
+    global to_delete
+    to_delete = "explanation"
 
 def delete_one():
-    lb_explanation.delete("active")
-    print(explanation)
+    print("DELETE ONE")
+    global to_delete
+    print(to_delete)
+    if to_delete == "definition":
+        lbl_definition["text"] = ""
+    elif to_delete == "explanation":
+        lbl_explanation["text"] = ""
+    elif to_delete == "example":
+        lbl_example["text"] = ""
 
 def show_concept_one():
-    change_graph("monopoly_profit.gif")
-    lb_explanation.delete(0, "end")
+    change_graph("monopoly.gif")
     for concept in concepts:
-        print("Definition: {}".format(concept.definition))
-        print("Explanation: {}".format(concept.explanation))
-        print("Example: {}".format(concept.example))
-    lb_explanation.insert("end", concept_1.definition, concept_1.explanation, concept_1.example)
+        definition = "{}".format(concept.definition)
+        explanation = "{}".format(concept.explanation)
+        example = "{}".format(concept.example)
+    lbl_definition["text"] = concept_1.definition
+    lbl_explanation["text"] = concept_1.explanation
+    lbl_example["text"] = concept_1.example
+
 
 
 def show_concept_two():
-    lb_explanation.delete(0, "end")
     for concept in concepts:
-        print("Definition: {}".format(concept.definition))
-        print("Explanation: {}".format(concept.explanation))
-        print("Example: {}".format(concept.example))
-    lb_explanation.insert("end", concept_2.definition, concept_2.explanation, concept_2.example)
-
-def show_concept_three():
-    lb_explanation.delete(1, "end")
-    for concept in concepts:
-        print("Definition: {}".format(concept.definition))
-        print("Explanation: {}".format(concept.explanation))
-        print("Example: {}".format(concept.example))
-    lb_explanation.insert("end", concept_3.definition, concept_3.explanation, concept_3.example)
+        definition = "{}".format(concept.definition)
+        explanation = "{}".format(concept.explanation)
+        example = "{}".format(concept.example)
+    lbl_definition["text"] = concept_2.definition
+    lbl_explanation["text"] = concept_2.explanation
+    lbl_example["text"] = concept_2.example
 
 def load_file():
     data = pickle.load(open("Capstone.dat", "rb"))
@@ -98,40 +107,48 @@ btn_concept_1.grid(row = 1, column = 0)
 btn_concept_2 = tkinter.Button(root, text = "Game Theory", fg = "black", width = 16, height = 3, command = show_concept_two) 
 btn_concept_2.grid(row = 2, column = 0)
 
-btn_concept_3 = tkinter.Button(root, text = "Marginal Analysis", fg = "black", width = 16, height = 3, command = show_concept_three) 
-btn_concept_3.grid(row = 3, column = 0)
 
 btn_delete_one = tkinter.Button(root, text = "Delete One", fg = "black", width = 16, height = 3, command = delete_one) 
-btn_delete_one.grid(row = 4, column = 0)
+btn_delete_one.grid(row = 3, column = 0)
 
 btn_save_file = tkinter.Button(root, text = "Save File", fg = "black", width = 16, height = 3, command = save_file) 
-btn_save_file.grid(row = 5, column = 0)
+btn_save_file.grid(row = 4, column = 0)
 
 btn_load_file = tkinter.Button(root, text = "Load File", fg = "black", width = 16, height = 3, command = load_file) 
-btn_load_file.grid(row = 6, column = 0)
+btn_load_file.grid(row = 5, column = 0)
 
 btn_exit = tkinter.Button(root, text = "Exit", fg = "black", width = 16, height = 3, command = exit) 
-btn_exit.grid(row = 7, column = 0)
+btn_exit.grid(row = 6, column = 0)
 
-lbl_title = tkinter.Label(root, text = "Countdown Dates", bg = "White")
-lbl_title.grid(row = 8, column = 0)
+lbl_title = tkinter.Label(root, text = "Dates of Test: May 17 2019", bg = "White")
+lbl_title.grid(row = 7, column = 0)
 
-lb_dates = tkinter.Listbox(root, height = 2) 
-lb_dates.grid(row = 8, column = 0)
+lbl_e = tkinter.Label(root, text = "Explanation", bg = "White")
+lbl_e.grid(row = 0, column = 4)
 
-lbl_explanation = tkinter.Label(root, text = "Explanation", bg = "White")
-lbl_explanation.grid(row = 0, column = 4)
-
-lb_explanation = tkinter.Listbox(root, width = 100, height = 48) 
-lb_explanation.grid(row = 1, column = 4, rowspan = 9)
+lbl_definition = tkinter.Label(root, text = "" , bg = "white", width = 100)
+lbl_definition.grid(row = 2, column = 4)
+ 
+lbl_explanation = tkinter.Label(root, text = "" , bg = "white", width = 100)
+lbl_explanation.grid(row = 4, column = 4)
+ 
+lbl_example = tkinter.Label(root, text = "" , bg = "white", width = 100)
+lbl_example.grid(row = 6, column = 4)
  
 photo = tkinter.PhotoImage()
 lb_graph = tkinter.Label(image = photo)
 lb_graph.image = photo 
-lb_graph.grid(row = 0, column = 8, rowspan = 5)
+lb_graph.grid(row = 0, column = 8, rowspan = 7)
 
 text_input = tkinter.Text(root, width = 58, bg="Yellow", bd=5)
 text_input.grid(row = 7, column = 8, rowspan = 2) 
+
+lbl_definition.bind("<Button-1>", set_delete_definition)
+
+lbl_explanation.bind("<Button-1>", set_delete_explanation)
+
+lbl_example.bind("<Button-1>", set_delete_example )
+
 
 
 root.mainloop() 
